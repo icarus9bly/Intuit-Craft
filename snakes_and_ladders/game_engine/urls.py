ab99457f66ff237facf52fbc989ecbae44f7bb0d
Aditya Pratap Singh <adinick333@gmail.com>
2021-08-15 13:02:10 +0530

    added game_engine

--- snakes_and_ladders/game_engine/urls.py
+++ snakes_and_ladders/game_engine/urls.py
@@ -1,4 +1,4 @@
-"""snakes_and_ladders URL Configuration
+"""Game engine URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,10 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path
+from .views import index
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('game_engine.urls'))
+    path('',index),
 ]
