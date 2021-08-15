a621f83936a5db0d70c5b7462d66872e1578336b
Aditya Pratap Singh <adinick333@gmail.com>
2021-08-15 13:30:08 +0530

    added static

--- snakes_and_ladders/game_engine/views.py
+++ snakes_and_ladders/game_engine/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "game_engine/index.html")
+    return render(request, "game_engine/game_canvas.html")
