eb147223585199f19525a59b3dba17c32e8f1493
Aditya Pratap Singh <adinick333@gmail.com>
2021-08-16 23:01:43 +0530

    switching debug to false

--- snakes_and_ladders/snakes_and_ladders/settings.py
+++ snakes_and_ladders/snakes_and_ladders/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w%8ux)71yqqry155z^4l@8lo4!zj)-kzlexkub-gevl2xso*3('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -122,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
