32690745b4c6ea1c90b5387a14082aac9d604a9a
Aditya Pratap Singh <adinick333@gmail.com>
2021-08-17 07:31:38 +0530

    debug vals

--- snakes_and_ladders/game_engine/views.py
+++ snakes_and_ladders/game_engine/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, HttpResponse
 from game_engine.loggers import mana_logger
 # Create your views here.
 def no_js(request):
+    mana_logger.info('No js view is being used')
     return render(request, "game_engine/no_js.html")
 
 def basic_grid(request):
+    mana_logger.info('Basic grid is being used')
     return render(request, "game_engine/basic_grid.html")    
 
 def all_server_side(request):
